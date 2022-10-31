5ce1df80356c0c2c594053d1b4dd853fd1eb4432
Claire Devlin <claire.devlin@hotmail.com>
2022-10-31 12:30:33 +0100

    initial commit: NextJS front end

---
+++ react-front/pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Thanya Iyer - rest" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+
+        <h1>Thanya Iyer | rest</h1>
+        working
+
+
+
+      </main>
+
+
+    </div>
+  )
+}
