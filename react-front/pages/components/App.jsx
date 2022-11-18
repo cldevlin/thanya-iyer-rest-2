811df580e700de150e72b363edc0bacc64e90253
Claire Devlin <claire.devlin@hotmail.com>
2022-11-18 12:58:27 -0500

    implement handlePrevious button & disable buttons during transitions & add icons

--- react-front/pages/components/App.jsx
+++ react-front/pages/components/App.jsx
@@ -5,6 +5,14 @@ import leftArrowBird from '../../public/left-arrow-bird.png'
 import rightArrowFlower from '../../public/right-arrow-flower.png'
 import eye from '../../public/eye.png'
 
+//dimensions: w 200 x  h 232
+import blueYellowBird from '../../public/weird-blue-and-yellow-bird.png'
+
+//dimensions: w 200 x h 196
+import pinkDaisyFlower from '../../public/pink-daisy-flower.png'
+
+//deminsions: 200 x 219
+import redLotus from '../../public/red-lotus.png'
 const videoUrls = [
   'videos/1_instrumentals_intro.mp4',
   'videos/2_Slow_burn.mp4',
@@ -35,10 +43,16 @@ const App = () => {
   const [currentVideo, setCurrentVideo] = useState(null);
   const [videoOpacities, setVideoOpacities] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
+  const [buttonsAreDisabled, setButtonsAreDisabled] = useState(true)
+  const [readyCount, setReadyCount] = useState(0);
+
+  console.log('readyCount :>> ', readyCount);
 
   const handleNext = (e) => { 
-    //disable next & previous buttons
-    console.log('currentVideo :>> ', currentVideo);
+    if (buttonsAreDisabled) {
+      return;
+    }
+    setButtonsAreDisabled(true);
 
     const currentVideoIndex = currentVideo;
     const nextVideoIndex = currentVideo < videoUrls.length - 1 ? currentVideo + 1 : 0;
@@ -48,8 +62,26 @@ const App = () => {
     setTimeout(() => {
       pauseAndReset(currentVideoIndex)
       setCurrentVideo(nextVideoIndex);
+      setButtonsAreDisabled(false)
     }, 3000)
+  }
 
+  const handlePrevious = (e) => {
+    if (buttonsAreDisabled) {
+      return;
+    }
+    setButtonsAreDisabled(true)
+
+    const currentVideoIndex = currentVideo;
+    const nextVideoIndex = currentVideo > 0 ? currentVideo - 1 : videoUrls.length - 1;
+
+    playVideo(nextVideoIndex)
+    doVideoTransition(currentVideo, nextVideoIndex);
+    setTimeout(() => {
+      pauseAndReset(currentVideoIndex)
+      setCurrentVideo(nextVideoIndex);
+      setButtonsAreDisabled(false)
+    }, 3000)
   }
 
   const pauseAndReset = (index) => {
@@ -60,24 +92,35 @@ const App = () => {
 
   const playVideo = (index) => {
     const vid = document.getElementById(`react-video-${index}`);
+    vid.volume = 1;
     vid.play()
   }
 
   const doVideoTransition = (currentVideoIndex, nextVideoIndex) => {
-    // audio fade
+    // audio fade 3 seconds
     fadeOutVolume(currentVideo);
 
-    // visual fade:
+    // visual fade 3 seconds
     const newVideoOpacities = [...videoOpacities];
     newVideoOpacities[currentVideoIndex] = 0;
     newVideoOpacities[nextVideoIndex] = 1;
     setVideoOpacities(newVideoOpacities);
   }
 
+  const handleVideoReady = (index) => {
+    if (index === 0 && currentVideo === null) {
+      handleFirstVidReady()
+    }
+    setReadyCount(prev => prev + 1)
+  }
+
   const handleFirstVidReady = () => {
     playVideo(0);
     setVideoOpacities([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     setCurrentVideo(0)
+    setTimeout(() => {
+      setButtonsAreDisabled(false)
+    }, 3000)
   }
 
 
@@ -93,14 +136,6 @@ const App = () => {
     }, 300);
   }
 
-  //function that switches to previous video
-  const handlePrevious = (e) => {
-    if (currentVideo > 0) {
-      setCurrentVideo(prev => prev - 1)
-    } else {
-      setCurrentVideo(videoUrls.length - 1)
-    }
-  }
 
   const handleBegin = () => {
     setHasBegun(true)
@@ -112,48 +147,97 @@ const App = () => {
         <Image className="begin-button" alt="eye" src={eye} onClick={handleBegin} priority />
       }
       {hasBegun &&
-        <>
-        <div className="buttons-and-video-wrapper">
+        <div>
+          <div className="animated-icons left">
+            <Image className="blue-yellow-bird" src={blueYellowBird} alt="" />
+            <Image className="pink-daisy-flower" src={pinkDaisyFlower} alt="" />
 
-          <Image className="image-button left-arrow" src={leftArrowBird} alt="left arrow bird" width="300px" height="200px" onClick={handlePrevious} />
-
-        <div className="player-wrapper">
-            {videoUrls.map((url, index) => {
-              return (
-                <video
-                  src={url}
-                  key={url}
-                  className="react-player"
-                  id={`react-video-${index}`}
-                  style={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    zIndex: index,
-                    transition: 'opacity 5s',
-                    opacity: `${videoOpacities[index]}`,
-                    width: `${WIDTH}px`,
-                    height: `${HEIGHT}px`,
-                  }}
-                  loop
-                  onCanPlayThrough={() => index === 0 && currentVideo === null && handleFirstVidReady()}
-
-                  onPlay={() => console.log('playing #', index + 1)}
-                  onStalled={() => console.log('stalled #', index + 1)}
-                />
-              )
-            })
-
-            }
           </div>
-          <Image className="image-button right-arrow" src={rightArrowFlower} alt="right arrow flower" width="300px" height="175px" onClick={handleNext} style={{ float: 'right' }} />
-        </div>
 
-        </>
+          <div className="animated-icons right">
+            <Image className="red-lotus" src={redLotus} alt="" />
+
+          </div>
+
+
+          <div className="buttons-and-video-wrapper">
+
+            <Image className="image-button left-arrow" src={leftArrowBird} alt="left arrow bird" width="300px" height="200px" onClick={handlePrevious} />
+
+            <div className="player-wrapper">
+              {videoUrls.map((url, index) => {
+                return (
+                  <video
+                    src={url}
+                    key={url}
+                    className="react-player"
+                    id={`react-video-${index}`}
+                    style={{
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                      zIndex: index,
+                      transition: 'opacity 5s',
+                      opacity: `${videoOpacities[index]}`,
+                      width: `${WIDTH}px`,
+                      height: `${HEIGHT}px`,
+                    }}
+                    loop
+                    onCanPlayThrough={() => handleVideoReady(index)}
+
+                    onPlay={() => console.log('playing #', index + 1)}
+                    onStalled={() => console.log('stalled #', index + 1)}
+                  />
+                )
+              })
+
+              }
+            </div>
+
+            <Image className="image-button right-arrow" src={rightArrowFlower} alt="right arrow flower" width="300px" height="175px" onClick={handleNext} style={{ float: 'right' }} />
+
+          </div>
+        </div>
       }
+
       <style>
         {`
+          .animated-icons {
+            position: absolute;
+
+            height: 100vh;
+            width: 10vw;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            // border: black 5px solid;
+          }
+
+          .animated-icons.left {
+            left: 0;
+            top: 0;
+          }
+              .blue-yellow-bird {
+                // position: relative;
+                // left: 50px;
+                // top: 50px;
+                // width: 50%;
+                // height: 50%;
+              }
+              .pink-daisy-flower {
+                // position: relative;
+                // left: 50px;
+                // bottom: 50px;
+              }
+          .animated-icons.right {
+            right: 0;
+            top: 0;
+          }
+              .red-lotus {
+                margin-right: 10px
+              }
+
           .buttons-and-video-wrapper {
             width: ${WIDTH}px;
             display: flex;
